4dbfef0ec1df40a20a6f3b227254d49c93f0aa2f
shaikouskiarka <arkadiy92@mail.ru>
2015-04-08 22:03:50 +0300

    stage1
    fucking project

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Helpers;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = File.ReadAllText(@"name.txt");
+            dynamic obj = Json.Decode(s);
+            label1.Text = obj.Status;
+            Console.WriteLine(obj.Status + ", " + obj.Channel_type);
+            Console.ReadLine();
+        }
+    }
+}
